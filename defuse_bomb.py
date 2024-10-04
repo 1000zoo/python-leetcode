f1b5439ae02a31d6b2bcd78f8a3c152664518fd4
1000zoo <cjswldn99@gmail.com>
2024-10-04 13:18:15 +0900

    leetcode / easy / defuse the bomb

---
+++ defuse_bomb.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    """
+    Runtime 44 ms Beats 6.25%
+    """
+    def solution1(self, code, k):
+        if k == 0:
+            return [0 for _ in code]
+
+        def next_index(i, length, sign):
+            n = i + sign
+            if n < 0:
+                return length + n
+            elif n >= length:
+                return n - length
+            else:
+                return n
+        answer = []
+        sign = abs(k) // k
+        length = len(code)
+
+        for i in range(length):
+            curr = i
+            temp = 0
+            for _ in range(abs(k)):
+                curr = next_index(curr, length, sign)
+                temp += code[curr]
+            answer.append(temp)
+
+        return answer
