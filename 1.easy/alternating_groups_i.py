d53d31a01244eea08c16f1252103ebd69b26f72a
1000zoo <cjswldn99@gmail.com>
2024-10-21 15:32:42 +0900

    leetcode / easy / 3206

---
+++ 1.easy/alternating_groups_i.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    """
+    Runtime 52 ms Beats 54.59%
+    """
+    def numberOfAlternatingGroups(self, colors: list[int]) -> int:
+        answer = 0
+        
+        for i in range(len(colors)):
+            c1 = colors[i]
+            c2 = colors[(i + 1) % len(colors)]
+            c3 = colors[(i + 2) % len(colors)]
+            if c1 == c3 and c1 != c2:
+                answer += 1
+
+        return answer
