bcbd6e2921b974aab010d5a2904ce2075fda561b
1000zoo <cjswldn99@gmail.com>
2024-11-27 12:08:36 +0900

    leetcode / medium / 930

--- 2.medium/binary_subarrays_with_sum.py
+++ 2.medium/binary_subarrays_with_sum.py
@@ -20,3 +20,25 @@ class Solution:
             return answer
 
         return helper(goal) - helper(goal + 1)
+    
+    """
+    Runtime 25 ms Beats 86.53%
+    """
+    def prefixSumSolution(self, nums: list[int], goal: int) -> int:
+        from collections import defaultdict
+
+        answer = 0
+        prefix = 0
+        table = defaultdict(int)
+        table[0] = 1
+
+        for num in nums:
+            prefix += num
+
+            diff = prefix - goal
+            answer += table[diff]
+
+            table[prefix] += 1
+            
+        return answer
+            
